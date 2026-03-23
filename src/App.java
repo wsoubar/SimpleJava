07a6640f5f1e9efcc42c72f79219dea9395b16e2
wsoubar <wsoubar@gmail.com>
2026-03-23 01:52:41 -0300

    ajuste  @Slf4j

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class App {
     
-    private static final Logger log = LoggerFactory.getLogger(App.class);
+    //private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws Exception {
         log.info("Hello, World!");
